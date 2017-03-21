e0245977ff20148e576657af4e5168cfc61a4491
JC <JC@ZNIZNE>
2017-03-21 18:11:32 +0800

    Add project files.

---
+++ GigHub/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
